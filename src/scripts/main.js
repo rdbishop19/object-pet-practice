6f34694a2edb1d2e43f3492fec0f17b9eebecb64
Ryan Bishop <rdbishop06@gmail.com>
2019-10-11 13:25:35 -0500

    Pet object initialization

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
-console.log("PING");
+// console.log("PING");
+
+// Create an object that represents your pet.
+
+const myPet = {
+    // Name property with a string value.
+    name: "Maggie",
+    // Species property with a string value.
+    species: "Cat",
+    // Nicknames property with an array value. Array contains strings.
+    nicknames: ["Maggie-Moo", "Mags-Pie", "Kitten Pants", "Chick-Pie"],
+    // Age property with an integer value
+    age: 8
+}
+
+console.log(myPet);
