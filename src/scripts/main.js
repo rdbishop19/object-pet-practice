cd9d4d07d4c41a203cf87e0fad5c9366beba2bc8
Ryan Bishop <rdbishop06@gmail.com>
2019-10-11 13:31:10 -0500

    Pet sounds added and invoked successfully

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,6 @@
 // console.log("PING");
 
 // Create an object that represents your pet.
-
 const myPet = {
     // Name property with a string value.
     name: "Maggie",
@@ -10,7 +9,22 @@ const myPet = {
     // Nicknames property with an array value. Array contains strings.
     nicknames: ["Maggie-Moo", "Mags-Pie", "Kitten Pants", "Chick-Pie"],
     // Age property with an integer value
-    age: 8
+    age: 8,
+    // Pick three behaviors that your pet has and add three keys to your pet object. 
+    hiss: function(){
+        window.alert("**CAT HISSING**");
+    },
+    meow: function(){
+        window.alert("MOWWWWWWWWWWWW");
+    },
+    sleep: function(){
+        window.alert("zzzzzZZZZZZZZZZ");
+    }
 }
 
 console.log(myPet);
+
+//Once you have define the properties, invoke each behavior.
+myPet.hiss();
+myPet.meow();
+myPet.sleep();
